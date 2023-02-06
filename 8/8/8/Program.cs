d208d6810b67556d212e32f2c2fe414fedd2e1c1
MrNadie032 <123603722+MrNadie032@users.noreply.github.com>
2023-02-06 14:37:18 -0400

    actualizacion nuevo codigo y correciones

---
+++ 8/8/8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULO DE ECUACIONES DE 2DO GRADO");
+            Console.WriteLine("INGRESE LA SIGUIENTES VARIABLES: ");
+            Console.WriteLine("A: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("B: ");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("C: ");
+            int C = int.Parse(Console.ReadLine());
+
+
+            double Parte1 = Math.Pow(B, 2) - 4 * (A * C);
+            double X1, X2;
+
+            if (Parte1 > 0)
+            {
+                X1 = -B + Math.Sqrt(Parte1) / (2 * A);
+                X2 = -B - Math.Sqrt(Parte1) / (2 * A);
+                Console.WriteLine("LA RESPUESTA ES PARA LA VARIABLE 1: " + X1 + '\n' + "LA RESPUESTA DE LA VARIABLE 2: " + X2);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("NO EXISTE SOLUCION REAL MI BROTHER");
+                Console.ReadLine();
+            }
+
+
+
+
+        }
+    }
+}
