015438b26c26196f3bc87451a41787f68c8e1e5a
MrNadie032 <123603722+MrNadie032@users.noreply.github.com>
2023-02-07 19:29:34 -0400

    .

--- 5/5/5/Program.cs
+++ 5/5/5/Program.cs
@@ -10,59 +10,137 @@ namespace _5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("BIENVENIDOS A CALCULOS CON VOLUMEN DE FIGURA GEOMETRICAS");
-            Console.WriteLine("Las opciones para calcular son: ");
-            Console.WriteLine("1 - CUBO");
-            Console.WriteLine("2 - ESFERA");
-            Console.WriteLine("3 - CONO");
-            Console.WriteLine("4 - PIRAMIDE");
-            Console.WriteLine("SELECIONES LA FIGURA QUE DESEE CALCULAR");
-            int opciones = int.Parse(Console.ReadLine());
-            switch (opciones)
+            Console.WriteLine("BIENVENIDOS A CALCULOS CON FRACCIONES");
+            Console.WriteLine("ELIGE UNA OPCION");
+            Console.WriteLine("1-SUMA");
+            Console.WriteLine("2-RESTA");
+            Console.WriteLine("3-MULTIPLICACION");
+            Console.WriteLine("4-DIVISION");
+
+            int i = int.Parse(Console.ReadLine());
+
+            switch (i)
             {
                 case 1:
-                    Console.WriteLine("La ecuacion es VOLUMEN = LADO^3");
-                    Console.WriteLine("ingrese la cantidad de lados: ");
-                    double h = int.Parse(Console.ReadLine());
-                    double n = Math.Pow(h, 3);
-                    Console.WriteLine("El RESULTADO DEL CALCULO ES: " + n);
-                    Console.ReadLine();
+
+                    Console.WriteLine("Ingrese su primer numerador: ");
+                    int num1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese su primer denominador: ");
+                    int dem1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Su fraccion es: " + num1 + "/" + dem1);
+
+                    Console.WriteLine("Ingrese su segundo numerador: ");
+                    int num2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese su segundo denominador: ");
+                    int dem2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Su fraccion es: " + num2 + "/" + dem2);
+
+                    if (dem1 == dem2)
+                    {
+                        int numis = num1 + num2;
+
+                        Console.WriteLine("La suma de fracciones es: " + numis + "/" + dem1);
+                    }
+                    else
+                    {
+                        int numdif = ((num1 * dem2) + (num2 * dem1));
+                        int demdif = dem1 * dem2;
+
+                        Console.WriteLine("La suma de fracciones es: " + numdif + "/" + demdif);
+                    }
+
                     break;
+
                 case 2:
-                    Console.WriteLine("La ecuacion es volumen = 4/3 * π * Radio^3");
-                    Console.WriteLine("ingrese el radio");
-                    double y = double.Parse(Console.ReadLine());
-                    double f = (4 / 3) * Math.PI * Math.Pow(y, 3);
-                    Console.WriteLine("El RESULTADO DEL CALCULO ES: " + f);
-                    Console.ReadLine();
+                    Console.WriteLine("Ingrese su primer numerador: ");
+                    num1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nIngrese su primer denominador: ");
+                    dem1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nSu fraccion es: " + num1 + "/" + dem1);
+
+                    Console.WriteLine("\nIngrese su segundo numerador: ");
+                    num2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nIngrese su segundo denominador: ");
+                    dem2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nSu fraccion es: " + num2 + "/" + dem2);
+
+                    if (dem1 == dem2)
+                    {
+                        int numis = num1 - num2;
+
+                        Console.WriteLine("\nLa resta de fracciones es: " + numis + "/" + dem1);
+                    }
+                    else
+                    {
+                        int numdif = ((num1 * dem2) - (num2 * dem1));
+                        int demdif = dem1 * dem2;
+
+                        Console.WriteLine("\nLa resta de fracciones es: " + numdif + "/" + demdif);
+                    }
                     break;
+
                 case 3:
-                    Console.WriteLine("La ecuacion es VOLUMEN = 1/3 * ALTURA * π * RADIO^2");
-                    Console.WriteLine("ingrese la ALTURA");
-                    double e = double.Parse(Console.ReadLine());
-                    Console.WriteLine("ingrese la RADIO");
-                    double j = double.Parse(Console.ReadLine());
-                    double q = (1.0 / 3.0) * e * Math.PI * (Math.Pow(j, 2));
-                    Console.WriteLine("El RESULTADO DEL CALCULO ES: " + q);
-                    Console.ReadLine();
+                    Console.WriteLine("\nIngrese el primer numerador: ");
+                    num1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nIngrese el primer denominador: ");
+                    dem1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nSu fraccion es: " + num1 + "/" + dem1);
+
+                    Console.WriteLine("\nIngrese el segundo numerador: ");
+                    num2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nIngrese el segundo denominador: ");
+                    dem2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nSu fraccion es: " + num2 + "/" + dem2);
+
+                    int numr = num1 * num2;
+                    int demr = dem1 * dem2;
+                    Console.WriteLine("\nLa multiplicacion de fracciones es: " + numr + "/" + demr);
+
                     break;
 
                 case 4:
-                    Console.WriteLine("La ecuacion es VOLUMEN = BASE * ALTURA / 3");
-                    Console.WriteLine("ingrese la BASE");
-                    double O = double.Parse(Console.ReadLine());
-                    Console.WriteLine("ingrese la ALTURA");
-                    double L = double.Parse(Console.ReadLine());
-                    double b = ((O * L) / 3);
-                    Console.WriteLine("El RESULTADO DEL CALCULO ES: " + b);
-                    Console.ReadLine();
+                    Console.WriteLine("\nIngrese su primer numerador: ");
+                    num1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nIngrese su primer denominador: ");
+                    dem1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("su fraccion es: " + num1 + "/" + dem1);
+
+                    Console.WriteLine("\nIngrese su segundo numerador: ");
+                    num2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nIngrese su segundo denominador: ");
+                    dem2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nSu fraccion es: " + num2 + "/" + dem2);
+
+                    numr = num1 * dem2;
+                    demr = num2 * dem1;
+                    Console.WriteLine("\nLa division de fracciones es: " + numr + "/" + demr);
+
                     break;
 
                 default:
-                    Console.WriteLine("Opcion Invalida, aprende a leer");
-                    Console.ReadLine();
+                    Console.WriteLine("Opcion incorrecta");
                     break;
             }
+
+            Console.ReadLine();
         }
+        
     }
 }
+
